cbc14237c67951996ae67da743aa9d4dd2842528
Yaokai Liu <yaokai.liu@foxmail.com>
2024-12-04 00:14:05 +0800

    add 'Stack_empty'.

--- stack.c
+++ stack.c
@@ -19,7 +19,7 @@ struct Stack {
 #define ALLOC_LEN (32 * sizeof(void *))
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
-Stack *Stack_new(const Allocator *allocator) {
+inline Stack *Stack_new(const Allocator *allocator) {
   Stack *stack = allocator->malloc(sizeof(Stack));
   stack->allocator = allocator;
   stack->stack = allocator->malloc(ALLOC_LEN);
@@ -28,14 +28,14 @@ Stack *Stack_new(const Allocator *allocator) {
   return stack;
 }
 
-void Stack_clear(Stack *stack) {
+inline void Stack_clear(Stack *stack) {
   stack->allocator->free(stack->stack);
   stack->allocated = 0;
   stack->used = 0;
   stack->stack = nullptr;
 }
 
-uint32_t Stack_push(Stack *stack, const void *data, uint32_t size) {
+inline uint32_t Stack_push(Stack *stack, const void *data, uint32_t size) {
   if (!data || !size) { return 0; }
   if (stack->used + size >= stack->allocated) {
     uint32_t length = ((stack->used + size) / ALLOC_LEN + 1) * ALLOC_LEN;
@@ -49,15 +49,19 @@ uint32_t Stack_push(Stack *stack, const void *data, uint32_t size) {
   return size;
 }
 
-uint32_t Stack_pop(Stack *stack, void *dest, uint32_t size) {
+inline uint32_t Stack_pop(Stack *stack, void *dest, uint32_t size) {
   size = min(stack->used, size);
   stack->used -= size;
   if (dest) { memcpy(dest, stack->stack + stack->used, size); }
   return size;
 }
 
-uint32_t Stack_top(Stack *stack, void *dest, uint32_t size) {
+inline uint32_t Stack_top(Stack *stack, void *dest, uint32_t size) {
   size = min(stack->used, size);
   if (dest) { memcpy(dest, stack->stack + stack->used - size, size); }
   return size;
+}
+
+inline bool Stack_empty(Stack *stack) {
+  return !(stack->used > 0);
 }
--- stack.h
+++ stack.h
@@ -18,5 +18,5 @@ void Stack_clear(Stack *stack);
 uint32_t Stack_push(Stack *stack, const void *data, uint32_t size);
 uint32_t Stack_pop(Stack *stack, void *dest, uint32_t size);
 uint32_t Stack_top(Stack *stack, void *dest, uint32_t size);
-
+bool Stack_empty(Stack *stack);
 #endif  // MACHINE_STACK_H
